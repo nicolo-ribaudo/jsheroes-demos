a5a75a6b1bde5773b7d787da0146b0a81c17291a
Nicolò Ribaudo <hello@nicr.dev>
2024-05-24 10:53:42 +0300

    Updates

--- c/js/math.js
+++
@@ -1,3 +0,0 @@
-import buildModule from "../dist/math.mjs";
-
-export const { _fib: fib } = await buildModule();
--- stack-trace/dist/main.js
+++ stack-trace/dist/main.js
@@ -1,9 +1,9 @@
-class State {
+class S {
   get isLoggedIn() {return true;}
   get isAdmin() {return false;}
 }
 function n(state) {console.log("Is logged in?", state.isLoggedIn);return state.isLoggedIn && state.isAdmin;}
 
-const state = new State();
+const s = new S();
 console.log("Can read names?", n());
 //# sourceMappingURL=./main.js.map
